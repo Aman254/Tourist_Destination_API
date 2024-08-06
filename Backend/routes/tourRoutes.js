08cad40a2c7ae5bbb4dc3c3d8c3777cb88ebffe9
Aman254 <aman.mishra.2005@gmail.com>
2024-08-06 13:59:51 +0530

    Added Aggregation Pipeline

--- Backend/routes/tourRoutes.js
+++ Backend/routes/tourRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 const tourRoutes = require('../controllers/tourController');
 
+router.route('/tour-stats').get(tourRoutes.getTourStats);
+router.route('/monthly-plan/:year').get(tourRoutes.getMonthlyPlan);
+
 router
   .route('/top-5-cheap')
   .get(tourRoutes.aliasTopTours, tourRoutes.getAllTours);
